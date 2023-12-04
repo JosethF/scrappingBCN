793bfdfde1ece4c5acb279990e20e08c5a05a1d2
JosethF <hjcabanillas@gps.local>
2023-12-04 08:50:31 +0100

    v2?

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 from bs4 import BeautifulSoup
-import requests
-from datetime import datetime, timedelta
-"""
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-"""
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime, timedelta
+import time
+
 current_date = datetime.now().date()
 days_until_next_monday = (7 - current_date.weekday()) % 7
 next_monday_date = current_date + timedelta(days=days_until_next_monday)
 next_week_date = current_date + timedelta(days=7)
+
 url = f"https://www.eventbrite.es/d/spain--barcelona/free--events/?page=1&start_date={next_monday_date}&end_date={next_week_date}"
 print(url)
-"""
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get(url)
-"""
+time.sleep(10)
 
-html_text = requests.get(url, timeout=(20, 60)).text
-soup = BeautifulSoup(html_text,'lxml')
+html_text = driver.page_source
+driver.quit()
 
-titleHorizontal = soup.find_all('section', class_ = 'discover-horizontal-event-card')
+soup = BeautifulSoup(html_text, 'lxml')
+titleHorizontal = soup.find_all('section', class_='discover-horizontal-event-card')
 
 f = open("output.txt", "w")
 for card in titleHorizontal:
     element = card.div
-    badge = element.find_all('div', 'event-card-badge')
-    print(badge)
     link = element.a['href']
     title = element.h2.text
     paragraphsArray = list(element.find_all('p'))
@@ -38,6 +35,12 @@ for card in titleHorizontal:
         date = paragraphsArray[1].text
     else:
         date = paragraphsArray[0].text
-    line = f"{title} {date} {link}.\n"
-    #f.write(line)
+
+    badge = element.select_one('.event-card-badge')
+
+    badge_text = badge.text if badge else "No badge information"
+
+    line = f"{title} {date} {link} Badge: {badge_text}.\n"
+    f.write(line)
+
 f.close()
