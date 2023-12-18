1de88db7f18b8ef1db807ac935590b0aa4216583
JosethF <hjcabanillas@gps.local>
2023-12-18 21:28:28 +0100

    todo

--- main.py
+++ main.py
@@ -4,43 +4,54 @@ from selenium.webdriver.chrome.options import Options
 from datetime import datetime, timedelta
 import time
 
-current_date = datetime.now().date()
-days_until_next_monday = (7 - current_date.weekday()) % 7
-next_monday_date = current_date + timedelta(days=days_until_next_monday)
-next_week_date = current_date + timedelta(days=7)
+def scrape_page(driver, url):
+    driver.get(url)
+    time.sleep(10)
+    html_text = driver.page_source
+    soup = BeautifulSoup(html_text, 'lxml')
+    title_horizontal = soup.find_all('section', class_='discover-horizontal-event-card')
+    return title_horizontal
 
-url = f"https://www.eventbrite.es/d/spain--barcelona/free--events/?page=1&start_date={next_monday_date}&end_date={next_week_date}"
-print(url)
+def main():
+    current_date = datetime.now().date()
+    days_until_next_monday = (7 - current_date.weekday()) % 7
+    next_monday_date = current_date + timedelta(days=days_until_next_monday)
+    next_week_date = current_date + timedelta(days=7)
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
+    base_url = "https://www.eventbrite.es/d/spain--barcelona/free--events/?page={}&start_date={}&end_date={}"
 
-driver.get(url)
-time.sleep(10)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
 
-html_text = driver.page_source
-driver.quit()
+    page_number = 1
+    while True:
+        url = base_url.format(page_number, next_monday_date, next_week_date)
+        
+        title_horizontal = scrape_page(driver, url)
 
-soup = BeautifulSoup(html_text, 'lxml')
-titleHorizontal = soup.find_all('section', class_='discover-horizontal-event-card')
+        if not title_horizontal:
+            break  # No more pages
 
-f = open("output.txt", "w")
-for card in titleHorizontal:
-    element = card.div
-    link = element.a['href']
-    title = element.h2.text
-    paragraphsArray = list(element.find_all('p'))
-    if 'EventCardUrgencySignal__label' in paragraphsArray[0]['class']:
-        date = paragraphsArray[1].text
-    else:
-        date = paragraphsArray[0].text
+        with open("output.txt", "a") as f:
+            for card in title_horizontal:
+                element = card.div
+                link = element.a['href']
+                title = element.h2.text
+                paragraphs_array = list(element.find_all('p'))
+                date = paragraphs_array[1].text
+                if ':' in date.lower():
+                    line = f"{title} {date} {link}\n"
+                    f.write(line)
+                """
+                if 'EventCardUrgencySignal__label' in paragraphs_array[0]['class']:
+                    date = paragraphs_array[1].text
+                else:
+                """
 
-    badge = element.select_one('.event-card-badge')
+        #page_number += 1
 
-    badge_text = badge.text if badge else "No badge information"
+    driver.quit()
 
-    line = f"{title} {date} {link} Badge: {badge_text}.\n"
-    f.write(line)
-
-f.close()
+if __name__ == "__main__":
+    main()
